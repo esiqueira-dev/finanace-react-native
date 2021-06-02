51e88ff31f834847d4fd22e188c3ba52c8c4db70
Eduardo de Oliveira Siqueira <esiqueira.dev@gmail.com>
2021-06-02 15:46:34 -0300

    Add style and style props

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { HighlightCard } from '../../global/components/HighlightCard';
+import { HighlightCards } from '../../global/components/HighlightCard/styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  Username,
+  UserWrapper,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard3</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/80408016?v=4' }}
+            />
+            <User>
+              <UserGretting>Olá,</UserGretting>
+              <Username>Eduardo</Username>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saída"
+          amount="R$ 16.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 15.400,00"
+          lastTransaction="Última entrada dia 19 de abril"
+        />
+      </HighlightCards>
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,59 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { Feather } from '@expo/vector-icons'
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  color: ${({ theme }) => theme.colors.title};
-  font-size: 24px;
-  font-weight: bold;
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: row;
+`;
+
+export const UserInfo = styled.View `
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Photo = styled.Image `
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const User = styled.View `
+  margin-left: 17px;
+`;
+
+export const UserGretting = styled.Text `
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const Username = styled.Text `
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const UserWrapper = styled.View `
+  width: 100%;
+  padding: 0 ${RFValue(24)}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
